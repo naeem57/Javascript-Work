58abcf3a42fc47ec847f114fab8dae3414a61017
Muhammad Naeem <naeembangash57@gmail.com>
2024-12-15 12:31:46 +0000

    datatype conversion

---
+++ javascript/dataType_conversion.js
@@ -0,0 +1,28 @@
+let score = "33";
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//number 33 => string "33"
+//"33abc" = NaN
+
+let isLoggedIn = 1;
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanLoggedIn);
+//console.log(typeof booleanLoggedIn);
+//"" = false
+//"naeem" = true
+//true = 1, false = 0
+
+let numb = 33;
+
+let stringNumb = String(numb);
+console.log(stringNumb);
+console.log(typeof stringNumb);
+
+//numb 33 => string "33"
