c5e26dbffd3747c913e641e6e2ed55d63ac0cf31
Muhammad Naeem <naeembangash57@gmail.com>
2024-12-15 11:28:26 +0000

    variables done

---
+++ javascript/variables.js
@@ -0,0 +1,9 @@
+const accountId = 233454;
+let accountEmail = "naeembangash57@";
+var accountPassword = "224455";
+
+// accountId = 123456; cant change it 
+accountEmail = "khan123@";
+var accountPassword = "99999";
+
+console.table([accountId, accountEmail, accountPassword]);
