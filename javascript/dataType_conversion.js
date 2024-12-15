681003b7dcbad5284f08630405b472a233d1b90a
Muhammad Naeem <naeembangash57@gmail.com>
2024-12-15 17:56:13 +0000

    datatype and operation

--- javascript/dataType_conversion.js
+++ javascript/dataType_conversion.js
@@ -22,7 +22,37 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 let numb = 33;
 
 let stringNumb = String(numb);
-console.log(stringNumb);
-console.log(typeof stringNumb);
+// console.log(stringNumb);
+// console.log(typeof stringNumb);
 
 //numb 33 => string "33"
+
+
+// ************* OPERATIONS ****************
+ 
+let value = 3;
+let negNumb = -value;
+// console.log(negNumb);
+
+let two;
+
+let str1 = "hello";
+let str2 = " naeem";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(3 * 3);
+console.log(3 / 3);
+console.log(3 % 3);
+
+console.log(3 > 2);
+console.log(4 < 5);
+console.log(5 >= 5);
+console.log(6 <= 8);
+console.log(3 != 3);
+
+
+
+
+
