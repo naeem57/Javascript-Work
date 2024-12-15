b3f283aea28e5213b17801386b4410582e6ca5e3
Muhammad Naeem <naeembangash57@gmail.com>
2024-12-15 11:55:46 +0000

    datatype done

---
+++ javascript/DataTypes.js
@@ -0,0 +1,10 @@
+let name = "m naeem"; //string
+let age = 24; //int
+let isLogeIn = false; //boolean
+let state; //Undefined
+let city = null; //NUll
+
+//"symbol" use for unique
+//object 
+
+console.log(typeof city);
